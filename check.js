6a7094ff03a3610ba8539679f9fa279a0a6487ab
Semi_King <princexx345@gmail.com>
2023-11-03 11:17:24 +0200

    Wrote code for javascript validation and added an empty thank you page

--- check.js
+++ check.js
@@ -1,1 +1,25 @@
-// Code to validate form 
+// Code to validate form
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("form-group");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("inputEmail");
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue === "") {
+      alert("Please enter an email address");
+    }
+
+    // My Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailValue)) {
+        alert("Please enter a valid email address")
+        return;
+    }
+
+    // Show thank you page once all validation passes 
+
+  });
+});
